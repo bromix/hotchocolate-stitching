16ffd5fb42c8e17f5765fcc3c73683d0284b92aa
Matthias Bromisch <mbr@ashampoo.com>
2020-12-02 11:02:37 +0100

    ADD: stitching

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +12,11 @@ namespace hotchocolate_stitching
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient("almansi", c => c.BaseAddress = new Uri("https://graphqlzero.almansi.me/api"));
+        
+            services
+                .AddGraphQLServer()
+                .AddRemoteSchema("almansi");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +31,7 @@ namespace hotchocolate_stitching
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapGraphQL();
             });
         }
     }
